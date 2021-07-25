d1a3425900bab835421e9e057d17d769b834941f
H3R01A <aaronallen@Aarons-MBP.twmi.rr.com>
2021-07-25 18:15:00 -0400

    Update made

--- tower_of_god_ask.py
+++ tower_of_god_ask.py
@@ -6,10 +6,19 @@
 
 print("welcome to the Tower of God (ToG) Ask Game")
 print("Lets start with an easy question")
-answer = input("Who is your favorite character in ToG?")
+answer1 = input("Who is your favorite character in ToG?")
 
-if answer == "Bam":
+if answer1 == "Bam":
     print("That is AWESOME! He is the best so it makes sense")
 else:
     print("well...")
     print("Some of us just have bad taste I guess....")
+
+print("Next question for ya.\n")
+answer2 = input("Are you reading the Webtoon or did you watch the anime?\n (PLEASE ENTER EITHER: W (Webtoon) or A (Anime))")
+if answer2 == "W":
+    print("YES!!!! The current arc is something else right???")
+else:
+    print("Hey at least you watched the anime. Make sure to go read the Webtoon. IT ONLY GETS BETTER")
+
+
