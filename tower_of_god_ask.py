6327339e78221402c69eebdaea7ec0967b7838f7
H3R01A <aaronallen@Aarons-MBP.twmi.rr.com>
2021-07-25 18:02:20 -0400

    added opening and question about favorite character

--- tower_of_god_ask.py
+++ tower_of_god_ask.py
@@ -1,5 +1,15 @@
 #AreYouIntoToG Program Build a terminal program using Python from Computer Science
 
-print("Test")
-print("Bam is the best")
-print("Bam is OP")
+#print("Test")
+#print("Bam is the best")
+#print("Bam is OP")
+
+print("welcome to the Tower of God (ToG) Ask Game")
+print("Lets start with an easy question")
+answer = input("Who is your favorite character in ToG?")
+
+if answer == "Bam":
+    print("That is AWESOME! He is the best so it makes sense")
+else:
+    print("well...")
+    print("Some of us just have bad taste I guess....")
